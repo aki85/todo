43e14aeb4a26b673b0a75195bbdb87d92cd710a8
aki85 <akiaki8528@gmail.com>
2019-11-07 03:40:15 +0900

    small design

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Todo from './todo'
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Todo />, document.getElementById('root'));
--- src/todo.js
+++ src/todo.js
@@ -1,44 +1,48 @@
-import React, { Component } from 'react';
-import { Form, Button, InputGroup, FormControl } from 'react-bootstrap';
+import React, { Component } from 'react'
+import { Form, Button, InputGroup, FormControl } from 'react-bootstrap'
 export default class Todo extends React.Component {
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       todos: [],
       name: ''
-    };
+    }
   }
 
   onInput = (e) => {
+    if (30 < e.target.value.length) return;
     this.setState({
       name: e.target.value
-    });
+    })
   }
 
   addTodo = (e) => {
-    e.preventDefault();
-    const { todos, name } = this.state;
+    e.preventDefault()
+    const { todos, name } = this.state
+    if (!name) return
     this.setState({
-      todos: [...todos, name]
-    });
+      todos: [...todos, name],
+      name: ''
+    })
   }
 
   removeTodo = (index) => {
-    const { todos, name } = this.state;
+    const { todos, name } = this.state
     this.setState({
       todos: [...todos.slice(0, index), ...todos.slice(index + 1)]
-    });
+    })
   }
 
   render() {
-    const { todos } = this.state;
+    const { todos } = this.state
     return (<div className="todo">
       <header>Todo List</header>
       <div className="main">
         <Form onSubmit={this.addTodo.bind(this)}>
           <InputGroup className="mb-3">
             <FormControl
+              value={this.state.name}
               onInput={this.onInput}
             />
             <InputGroup.Append>
@@ -49,11 +53,11 @@ export default class Todo extends React.Component {
         <ul>
           {todos.map((todo, index) => <li key={index}>
             <span>{todo}</span>
-            <Button variant="danger" onClick={() => { this.removeTodo(index) }}>削除</Button>
+            <a tabIndex="-1" onClick={() => { this.removeTodo(index) }}><i className="fa fa-times"></i></a>
           </li>)}
         </ul>
       </div>
-      <footer>this is footer</footer>
-    </div>);
+      <footer>created by akic</footer>
+    </div>)
   }
 }
