bcdab39eac61c96867e758b7ec42534d3855c537
三輪一晶 <>
2019-12-04 13:32:15 +0900

    add spa redirect

--- src/admin.jsx
+++ src/admin.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Form, Button, InputGroup, FormControl } from 'react-bootstrap'
 import Todo from './todo'
 
 export default class Admin extends React.Component {
